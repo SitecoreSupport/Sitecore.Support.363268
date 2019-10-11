39ece6cfdc8ecaaa27d54f1f2e39ca56d0f99866
mao <mao@sitecore.net>
2019-10-11 23:36:30 +0300

    Add default implementation of ApplyFieldMappingRule class

---
+++ src/Sitecore.Support.363268/ContentSearch/SolrProvider/Pipelines/FormatQueryFieldValue/ApplyFieldMappingRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue
+{
+    public class ApplyFieldMappingRule : Sitecore.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue.ApplyFieldMappingRule
+    {
+        public ApplyFieldMappingRule()
+        {
+        }
+        
+        protected override string Escape(string fieldValue, string escapedValue, bool includeExistingSpecialCharacter, Collection<string> excludeEscapeCharacters, Collection<string> additionalEscapeCharacters = null)
+        {
+            HashSet<string> escapeCharacterSet = GetEscapeCharacterSet();
+            StringBuilder stringBuilder = new StringBuilder(50);
+            if (additionalEscapeCharacters != null)
+            {
+                escapeCharacterSet.UnionWith(additionalEscapeCharacters);
+            }
+            if (excludeEscapeCharacters != null)
+            {
+                escapeCharacterSet.ExceptWith(excludeEscapeCharacters);
+            }
+            foreach (string item in escapeCharacterSet)
+            {
+                stringBuilder.AppendFormat("\\{0}+|", item);
+            }
+            string text = stringBuilder.ToString();
+            
+            return new Regex(text.Remove(text.Length - 1)).Replace(fieldValue, (Match match) => escapedValue + (includeExistingSpecialCharacter ? match.Value : ""));
+        }
+    }
+}
